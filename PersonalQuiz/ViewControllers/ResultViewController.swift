361359ed898f37ebc5c92ec4ba174937b788d2a0
Julia Kazarina <yastrebovayv@gmail.com>
2023-04-08 19:07:29 +0500

    got the test result

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 final class ResultViewController: UIViewController {
-
+    
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var definitionResultLabel: UILabel!
+    
+    var answersChosen: [Answer]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
+        
+        let animalCounts = answersChosen.reduce(into: [:]) { counts, answerChosen in
+            counts[answerChosen.animal, default: 0] += 1
+        }
+        
+        if let resultAnimalName = animalCounts.max(by: { $0.value < $1.value })?.key {
+            resultLabel.text = "Вы — \(resultAnimalName.rawValue)"
+            definitionResultLabel.text = resultAnimalName.definition
+        } else {
+            resultLabel.text = ""
+            definitionResultLabel.text = ""
+        }
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
